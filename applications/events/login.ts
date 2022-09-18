cabc24ba979a6cf4bc5c236d9b2c6ec27d1155f6
doiská <doiskka@gmail.com>
2022-09-18 19:44:59 -0300

    (build) removed applications folder

--- applications/events/login.ts
+++
@@ -1,5 +0,0 @@
-export enum LoginEvents {
-	LOGIN_SCREEN = "server:login:show_screen",
-	CHARACTER_CUSTOMIZATION_SCREEN = "server:character_customization:show_screen",
-	CHARACTER_CUSTOMIZATION_SAVE = "server:character_customization:save",
-}
