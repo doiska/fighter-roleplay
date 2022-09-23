a4beb0449cbd3ec287296f292093b46d9f5f246c
doiská <doiskka@gmail.com>
2022-09-23 12:42:28 -0300

    (typings) added Core exp object to @fighter/typings

--- apps/appearance/fxmanifest.lua
+++ apps/appearance/fxmanifest.lua
@@ -2,21 +2,21 @@ fx_version 'cerulean'
 game 'gta5'
 name 'appearance'
 
-shared_script '@core/imports.lua'
+shared_script '@core/imports.js'
 
 server_script './server/server.js'
 client_script './client/client.js'
 
 files {
-  'web/index.html',
-  'web/assets/*.js',
-  'locales/*.json',
-  'peds.json',
-  'tattoos.json'
+    'web/index.html',
+    'web/assets/*.js',
+    'locales/*.json',
+    'peds.json',
+    'tattoos.json'
 }
 
 ui_page 'web/index.html'
 
 dependencies {
-  'core'
+    'core'
 }
--- apps/appearance/resources/server/server.ts
+++ apps/appearance/resources/server/server.ts
@@ -3,8 +3,14 @@ import { XiaoApplication } from "@fighter/framework";
 
 import { XiaoApp } from "@app/Xiao";
 
+// wait until Core is defined using Promise
+
 (async () => {
 	console.log("Xiao is running!");
 	await XiaoApplication.create(XiaoApp).then(app => app.start());
+
+	setTimeout(() => {
+		console.log(Core.getPlayers());
+	}, 1000);
 })();
 
